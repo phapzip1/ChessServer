ef0c8560740d70332a10d314465ac840bde66468
Phắc Nguyễn <phapdeptrai2k2@gmail.com>
2023-10-18 18:05:35 +0700

    fix test systax

--- tests/healthcheck.test.ts
+++ tests/healthcheck.test.ts
@@ -1,34 +1,17 @@
 import { createMock } from 'ts-auto-mock';
-import { On, method } from 'ts-auto-mock/extension';
 import { describe, expect, beforeEach, test } from '@jest/globals';
 import rpcHealthCheck from "../src/healthcheck";
 
 describe("rpcHealthCheck", function () {
-    let mockCtx: any, mockLogger: any, mockNk: any, mockLoggerError: any, mockNkStorageRead: any, mockNkStorageWrite: any, mockStorageWriteAck: any;
+    let mockCtx: any, mockLogger: any, mockNk: any;
     beforeEach(function () {
         mockCtx = createMock<nkruntime.Context>({ userId: "mock-user" });
         mockLogger = createMock<nkruntime.Logger>();
         mockNk = createMock<nkruntime.Nakama>();
-        mockStorageWriteAck = createMock<nkruntime.StorageWriteAck>();
-
-        mockLoggerError = On(mockLogger).get(method(function (mock) {
-            return mock.error;
-        }));
-
-        mockNkStorageRead = On(mockNk).get(method(function (mock) {
-            return mock.storageRead;
-        }));
-
-        mockNkStorageWrite = On(mockNk).get(method(function (mock) {
-            return mock.storageWrite;
-        }));
-
     });
 
-    test('returns failure if payload is null', function () {
-        const payload = null;
+    test("Return healthCheck: OK", function () {
         const result = rpcHealthCheck(mockCtx, mockLogger, mockNk, "");
-        const expectedError = 'no payload provided';
 
         expect(result).toBe(JSON.stringify({ healthcheck: "OK" }));
     });
