15154c4f882d5c505a7acc04c3c93711a49cc3a0
Phắc Nguyễn <phapdeptrai2k2@gmail.com>
2023-10-22 16:54:27 +0700

    fix before hook

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,5 +1,6 @@
 const bAuthenticationCustom = function(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, data: any) : any | void {
     logger.info(`Before authentication hook call ${data}`);
+    return data;
 }
 
 export {
--- src/realtime.ts
+++ src/realtime.ts
@@ -1,5 +1,6 @@
 const bPartyJoin = function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, envelope: any): any | void {
     logger.debug(`Join party called ${ctx.userId}`);
+    return envelope;
 }
 
 export {
